7a554b5babe833dfa5f683f4e59e5ebf4621b6f4
bhumikakelkar <159790025+bhumikakelkar@users.noreply.github.com>
2024-02-14 12:27:25 +0000

    container config done

---
+++ 27th jan/1_variables.js
@@ -0,0 +1,20 @@
+const studRollno=123
+let studEmail="abc@gmail.com"
+var studPasswd="college"
+studCity="mumbai"
+let studState;
+// studRollno=1
+console.log(studRollno);
+
+
+studEmail="xyz@gmail.com"
+studPasswd="132345"
+studCity="Pune"
+
+// console.log(studEmail)
+// console.log(studPasswd)
+
+console.table([studEmail,studPasswd,studRollno,studState,studCity])
+/* Prefer not to use var
+because of issue in block scope & functional scope
+*/
---
+++ 27th jan/2_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//this treat all JS code as newer version
+
+// alert(3+3) we are  using nodejs,not browser hence this will not work as it works in browser
+
+console.log(3
+    *
+    3); //code credibility should be high
+
+console.log("Bhumika");
+
+let sname="Swati"
+let age=30
+let isLoggedIn=false
+let state;
+
+console.log(typeof state);
+
