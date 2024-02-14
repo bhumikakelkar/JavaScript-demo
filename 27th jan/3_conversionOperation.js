de7b17401e7bf93a26e2c5d8e768474299bd034e
bhumikakelkar <159790025+bhumikakelkar@users.noreply.github.com>
2024-02-14 12:56:32 +0000

    container comit done

---
+++ 27th jan/3_conversionOperation.js
@@ -0,0 +1,58 @@
+let score="123"
+
+console.log(typeof (score));
+
+let valueNumber=Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+//"22"  22
+// "123abc"=NaN  error
+// true=>1,false=>0  errror
+
+let someNumber="Bhumika"
+
+let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+//no point of typecasting str in str again
+
+
+/*************operations */ 
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);//this will not posssible in other languages
+
+// // console.log((2+5));
+// // console.log((2*5));
+// // console.log((2-5));
+// // console.log((5/2));
+// // console.log((5**2));
+// // console.log((5%2));
+
+
+// let str1="Bhumika"
+// let str2="Kelkar"
+// let fullName=str1+str2
+//let fullName=str1+" "+str2
+// console.log(fullName);
+
+// console.log("1"+2);
+// console.log("1"+2+2+"5");
+// console.log("1"+(2+2)+"5");
+// console.log((3+4)*5%3);
+// console.log((+true));//1
+//  console.log(+"");//0 false is taken
+
+// let num1,num2,num3
+//  num1=num2=num3=2+2
+// num1=num2=num3=(3+4)*5%3
+// console.table([num1,num2,num3]);
+
+// let gameCounter=100
+// gameCounter++ //pre increment
+// console.log(gameCounter);
+
+
+
